23c892f7910971ce4e97fa8a6538b811c583757b
HYSA <aj.anunciado@gmail.com>
2023-05-22 09:17:43 +0800

    first commit

---
+++ MemeGenerator.js
@@ -0,0 +1,22 @@
+const generateMemeBtn= document.querySelector(".meme-generator .generate-meme-btn");
+const memeImage = document.querySelector(".meme-generator img");
+const memeTitle = document.querySelector(".meme-generator .meme-title");
+const memeAuthor= document.querySelector("meme.generator .meme-author");
+
+const updateDetails= (url,title,author) =>{
+memeImage.setAttribute("src",url)
+memeTitle.innerHTML= title;
+meme.Author.innerHTML=author;
+
+}
+
+
+const generateMeme = ()=> {
+fetch("https://meme-api.com/gimme/Gunpla") .then(response =>response.json())
+.then(data => {
+    updateDetails(data.url, data.author,data.title)
+})
+
+}
+
+generateMemeBtn.addEventListener("click",generateMeme);
